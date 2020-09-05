ee77a6a06b90ccb501f742ae3e7c95a2f982020f
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-09-05 12:08:27 -0300

    Improving code style

--- R/get_dht11_sensor.R
+++ R/get_dht11_sensor.R
@@ -28,33 +28,48 @@ get_dht11_sensor <- function(pin) {
   new("DHT11", pin = pin, dht11 = config_dht11(as.integer(pin)))
 }
 
-#' @importFrom reticulate import py_install
 # @param pin An integer of length one with the GPIO pin that the sensor is
 #   connected to.
 config_dht11 <- function(pin) {
   # Try to load the Python libraries.
+  libs <- try_load(c("RPi.GPIO", "dht11"))
+
+  # Configure the initial GPIO status.
+  libs$gpio$setwarnings(FALSE)
+  libs$gpio$setmode(gpio$BCM)
+  # libs$gpio$cleanup()
+
+  # Create the DHT11 sensor Python instance.
+  libs$dht11$DHT11(pin = pin)
+}
+
+# Tries to load the desired Python libraries.
+#
+# It tries to load the desired Python libraries, if they are not installed, it
+# will ask to do so.
+#
+# @param py_libs A vector of Python packages names to install.
+#
+#' @importFrom reticulate import py_install
+#
+try_load <- function(py_libs) {
   imports_error <- try({
-    gpio <- import("RPi.GPIO")
-    dht11 <- import("dht11")
+    libs <- lapply(py_libs, import)
+    names(libs) <- py_libs
   })
   # If there was an error, then prompt to install the libraries.
   if (inherits(imports_error, "try-error")) {
     install_libs <- readline(paste0(
-      "This package requires the `RPi.GPIO` and `dht11` Python libraries to be installed.\n",
+      "This package requires the ",
+      py_libs,
+      " Python libraries to be installed.\n",
       "Would you like to install them? Input 'y' for true, other for false: "
     ))
     if (install_libs != "y")
       stop("Required libraries not installed.")
-    py_install(c("RPi.GPIO", "dht11"))
-    gpio <- import("RPi.GPIO")
-    dht11 <- import("dht11")
+    py_install(py_libs)
+    libs <- lapply(py_libs, import)
+    names(libs) <- py_libs
   }
-
-  # Configure the initial GPIO status.
-  gpio$setwarnings(FALSE)
-  gpio$setmode(gpio$BCM)
-  # gpio$cleanup()
-
-  # Create the DHT11 sensor Python instance.
-  dht11$DHT11(pin = pin)
+  libs
 }
